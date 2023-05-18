5e04d48f7fd78429dd296db7bcb1baed133e1579
int0p <closer.fz@gmail.com>
2023-05-18 12:01:59 +0900

    layout - daily weekly button

--- src/routes/daily/+page.svelte
+++ src/routes/daily/+page.svelte
@@ -9,7 +9,7 @@
     let currentDate = new Date();
     let month = currentDate.getMonth()+1;
     let day = currentDate.getDay();
-
+    let style_btnWeek = "w-[65px]";
 </script>
 
 <div class="flex-col m-4 h-screen w-[calc(100%-100px)]"> <!--    얘의 width가 full일 때 section모드에서 전체화면에 존재.  -->
@@ -19,14 +19,14 @@
         <Hr class="my-4 mx-auto md:my-8 " height="h-1">{month}월 {day}일</Hr>
 
         <!--    week -->
-        <div class="flex gap-2 mb-4 justify-center items-stretch">
-            <Button outline color="dark" size="md" >{day}, Mon</Button>
-            <Button outline color="dark" size="md" >{day+1}, Tue</Button>
-            <Button outline color="dark" size="md" >{day+2}, Wed</Button>
-            <Button outline color="dark" size="md" >{day+3}, Thu</Button>
-            <Button outline color="dark" size="md" >{day+4}, Fri</Button>
-            <Button outline color="blue" size="md">{day+5}, Sat</Button>
-            <Button outline color="red" size="md">{day+6}, Sun</Button>
+        <div class="flex gap-2 mb-4 justify-center items-stretch w-full">
+            <Button outline color="dark" size="sm" class={style_btnWeek}>{day}, Mon</Button>
+            <Button outline color="dark" size="sm" class={style_btnWeek}>{day+1}, Tue</Button>
+            <Button outline color="dark" size="sm" class={style_btnWeek}>{day+2}, Wed</Button>
+            <Button outline color="dark" size="sm" class={style_btnWeek}>{day+3}, Thu</Button>
+            <Button outline color="dark" size="sm" class={style_btnWeek}>{day+4}, Fri</Button>
+            <Button outline color="blue" size="sm"class={style_btnWeek}>{day+5}, Sat</Button>
+            <Button outline color="red" size="sm"class={style_btnWeek}>{day+6}, Sun</Button>
         </div>
     </div>
 
