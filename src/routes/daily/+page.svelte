7f3c032bdd03a4d0575b7a09afc3949d5ed5a730
int0p <closer.fz@gmail.com>
2023-05-18 12:34:14 +0900

    layout - daily week btn

--- src/routes/daily/+page.svelte
+++ src/routes/daily/+page.svelte
@@ -1,6 +1,6 @@
 <script>
     import {
-        Button, Hr,
+        Button, Hr,ButtonGroup,
     } from 'flowbite-svelte';
     import TimeRecord from './timeRecord.svelte';
     import MemoRecord from './memoRecord.svelte';
@@ -9,29 +9,41 @@
     let currentDate = new Date();
     let month = currentDate.getMonth()+1;
     let day = currentDate.getDay();
-    let style_btnWeek = "w-[65px]";
+    let style_btnWeek = "w-[80px] text-center uppercase ";
 </script>
 
 <div class="flex-col m-4 h-screen w-[calc(100%-100px)]"> <!--    얘의 width가 full일 때 section모드에서 전체화면에 존재.  -->
 <!--    date-->
-    <div class="flex-col h-[130px] w-full">
+    <div class="flex-col h-[140px] w-full ">
         <!--        today-->
-        <Hr class="my-4 mx-auto md:my-8 " height="h-1">{month}월 {day}일</Hr>
+        <Hr class="my-8 mx-auto " height="h-1">{month}월 {day}일</Hr>
 
         <!--    week -->
-        <div class="flex gap-2 mb-4 justify-center items-stretch w-full">
-            <Button outline color="dark" size="sm" class={style_btnWeek}>{day}, Mon</Button>
-            <Button outline color="dark" size="sm" class={style_btnWeek}>{day+1}, Tue</Button>
-            <Button outline color="dark" size="sm" class={style_btnWeek}>{day+2}, Wed</Button>
-            <Button outline color="dark" size="sm" class={style_btnWeek}>{day+3}, Thu</Button>
-            <Button outline color="dark" size="sm" class={style_btnWeek}>{day+4}, Fri</Button>
-            <Button outline color="blue" size="sm"class={style_btnWeek}>{day+5}, Sat</Button>
-            <Button outline color="red" size="sm"class={style_btnWeek}>{day+6}, Sun</Button>
+        <div class="flex-col ">
+            <div class="flex justify-center w-full gap-x-2">
+                <div class={style_btnWeek}>MON</div>
+                <div class={style_btnWeek}> TUE</div>
+                <div class={style_btnWeek}>WED</div>
+                <div class={style_btnWeek}>THU</div>
+                <div class={style_btnWeek}>FRI</div>
+                <div class="{style_btnWeek}, text-blue-700">SAT</div>
+                <div class="{style_btnWeek}, text-red-700">SUN</div>
+            </div>
+            <div class="flex justify-center w-full gap-x-2">
+                <Button outline color="dark" size="sm" class={style_btnWeek}>{day}</Button>
+                <Button outline color="dark" size="sm" class={style_btnWeek}>{day}</Button>
+                <Button outline color="dark" size="sm" class={style_btnWeek}>{day} </Button>
+                <Button outline color="dark" size="sm" class={style_btnWeek}>{day} </Button>
+                <Button outline color="dark" size="sm" class={style_btnWeek}>{day} </Button>
+                <Button outline color="blue" size="sm" class={style_btnWeek}>{day} </Button>
+                <Button outline color="red" size="sm" class={style_btnWeek}>{day} </Button>
+            </div>
         </div>
+
     </div>
 
 <!--    Record -->
-    <div class = "flex clear-both h-[calc(100%-150px)]">
+    <div class = "flex clear-both h-[calc(100%-165px)]">
         <TimeRecord class=""/>
         <MemoRecord class=" "/>
     </div>
