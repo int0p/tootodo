6143039b8fedb75df87992b683f6931ae86aec72
seungyeah <int0p@seungyeahui-MacBookPro.local>
2023-05-25 10:44:40 +0900

    build error- file name (대소문자)

--- src/routes/daying/work/+page.svelte
+++ src/routes/daying/work/+page.svelte
@@ -3,7 +3,7 @@
     export let showTimer;
     import Icon from '@iconify/svelte';
     import clock2Fill from '@iconify/icons-mingcute/clock-2-fill';
-    import WorkSelect from "./WorkSelect.svelte";
+    import WorkSelect from "./workSelect.svelte";
 </script>
 
 <div class="pt-8 pl-4 pr-4 pb-4 m-4 mt-0 flex gap-2">
