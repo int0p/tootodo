e8a0314bd76b71ac9ab8b6235b09c8703c39b84f
seungyeah <int0p@seungyeahui-MacBookPro.local>
2023-06-07 14:40:20 +0900

    daying - todolist design

--- src/lib/components/doneList.svelte
+++ src/lib/components/doneList.svelte
@@ -12,19 +12,19 @@
 <div class="w-full h-full relative border-l">
     <div class="flex justify-start relative">
         <div class="absolute w-3 h-3 bg-gray-200 rounded-full  -left-1.5 border border-white "></div>
-        <div class="text-sm font-normal leading-none text-gray-400 absolute left-3 ">Done List</div>
+        <div class="text-sm font-normal leading-none text-gray-400 absolute left-3 ">Do List</div>
     </div>
     <ul class="my-1 bg-white rounded-lg border border-gray-200 divide-y divide-gray-200
         h-[calc(100%-2.7rem)] w-[calc(100%-1rem)] overflow-y-scroll absolute right-0 top-5">
         {#if $todoList}
             {#each $todoList as {title,id} (id)}
-                <Checkbox custom class="cursor-pointer  peer-checked:border-blue-600 hover:text-gray-600 peer-checked:text-gray-600 hover:bg-gray-50"
+                <Checkbox custom class="cursor-pointer !border-t-0 peer-checked:border-blue-600 hover:text-gray-600 peer-checked:text-gray-600 hover:bg-gray-50"
                           bind:group value={title}>
-                    <li class="max-h-[64px] px-2 py-3 line-clamp-2 w-full ">
+                    <li class="max-h-[64px] px-2 py-3 line-clamp-2 w-full  border-b border-dashed ">
                         <div class="w-full relative space-x-2 flex justify-start items-centers w-max-[214px] ">
-                            <div class="self-center w-[3px] h-[20px] bg-blue-600 absolute left-0"></div>
+                            <div class="self-center w-[3px] h-full bg-blue-600 absolute left-0"></div>
                             <Checkbox checked class="self-center"/>
-                            <p class="text-[0.9rem] whitespace-normal max-w-[300px] overflow-x-hidden line-clamp-2">{title}</p>
+                            <p class="text-[0.9rem] max-w-[calc(100%-68px)] line-clamp-2">{title}</p>
                             <Icon icon={deleteOutlineRounded} width="22" class="h-full aspect-square self-center absolute right-0 bg-white"/>
                         </div>
                     </li>
@@ -34,4 +34,3 @@
         {/if}
     </ul>
 </div>
-
--- src/lib/components/todoList.svelte
+++ src/lib/components/todoList.svelte
@@ -18,18 +18,17 @@
         h-[calc(100%-1.7rem)] w-[calc(100%-1rem)] overflow-y-scroll absolute right-0 top-5">
         {#if $todoList}
             {#each $todoList as {title,id} (id)}
-                <Checkbox custom class="cursor-pointer  peer-checked:border-blue-600 hover:text-gray-600 peer-checked:text-gray-600 hover:bg-gray-50"
+                <Checkbox custom class="cursor-pointer !border-t-0 peer-checked:border-blue-600 hover:text-gray-600 peer-checked:text-gray-600 hover:bg-gray-50"
                           bind:group value={title}>
-                    <li class="max-h-[100px] px-2 py-3 line-clamp-2 w-full ">
-                        <div class="w-full relative space-x-3 flex justify-start items-centers w-max-[214px] ">
-                            <div class="self-center w-[4px] h-[20px] bg-blue-600 absolute left-0"></div>
-                            <Checkbox class="self-center"/>
-                            <p class="text-[1.1rem] whitespace-normal max-w-[300px] overflow-x-hidden line-clamp-2">{title}</p>
+                    <li class="max-h-[70px] px-2 py-3 w-full border-b border-dashed">
+                        <div class="w-full relative space-x-2.5 flex justify-start items-centers w-max-[214px]  ">
+                            <div class="self-center w-[4px] h-full bg-blue-600 absolute left-0"></div>
+                            <Checkbox class="self-center" />
+                            <p class="text-[1.1rem] w-full max-w-[calc(100%-68px)] line-clamp-2">{title}</p>
                             <Icon icon={deleteOutlineRounded} width="22" class="h-full aspect-square self-center absolute right-0 bg-white"/>
                         </div>
                     </li>
                 </Checkbox>
-
             {/each}
             <!--                <div class="my-2 border border-gray-200 dark:border-gray-700 rounded-lg p-2 w-44">{group}</div>-->
         {/if}
